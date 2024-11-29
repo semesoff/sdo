9608486b0742e0759afe65466aaaa6254f9ddff0
enter <entertoofficial@gmail.com>
2024-11-29 19:17:03 +0300

    update

--- app/config/config.py
+++ app/config/config.py
@@ -1,8 +1,9 @@
 import json
+import os
 
 def init_config() -> dict:
     try:
-        with open('app/config/config.json') as config_file:
+        with open(os.path.abspath("config/config.json")) as config_file:
             config: dict = json.load(config_file)
     except FileNotFoundError as e:
         raise FileNotFoundError(f"Config file not found. {e}")
